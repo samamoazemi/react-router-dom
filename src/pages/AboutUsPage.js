50814b1763b570c6cc91774454899b84787646ed
samamoazemi <moazemisama@gmail.com>
2021-12-20 01:47:27 -0800

    Develop general part of project

---
+++ src/pages/AboutUsPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutUs(props){
+    return(
+        <>
+        <p>اینجا صفحه درباره ما است</p>
+        <a href="/">go to home page</a>
+        </>
+    )
+}
+
+export default AboutUs;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage(props){
+    return(
+        <>
+        <p>اینجا صفحه خانه است</p>
+        </>
+    )
+}
+
+export default HomePage;
